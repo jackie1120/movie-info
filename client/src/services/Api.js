be2567a6ee84b648d49d3a265f9f2d4db8438ffd
jackie1120 <hao.huang@outlook.com>
2019-07-30 15:57:51 +0800

    [feat]添加axios拦截器，对所有请求和响应进行预处理

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,5 +1,48 @@
 import axios from 'axios'
+import { Loading } from 'element-ui'
+import store from '../store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
-const request = axios.create()
+const request = axios.create({
+  headers: {
+    showLoading: true
+  }
+})
+NProgress.configure({ showSpinner: false })
+
+request.interceptors.request.use(config => {
+  NProgress.start()
+  if (config.headers.showLoading) {
+    let loadingService = Loading.service({
+      text: '运行中',
+      // spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)',
+      fullscreen: true
+    })
+    store.dispatch('setLoadingService', loadingService)
+    delete config.headers.showLoading
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+request.interceptors.response.use(response => {
+  NProgress.done()
+  let loadingService = store.state.loadingService
+  if (loadingService) {
+    loadingService.close()
+  }
+  return response
+}, function (error) {
+  NProgress.done()
+  let loadingService = store.state.loadingService
+  if (loadingService) {
+    loadingService.close()
+  }
+  console.log(error)
+  return Promise.reject(error)
+})
 
 export default request
